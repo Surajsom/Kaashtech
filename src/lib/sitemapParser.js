350a0cb5e2408a4fc9fe4a064b86386ccf80bfe2
Surajsom <168538620+Surajsom@users.noreply.github.com>
2026-01-29 15:34:53 +0530

    Your descriptive commit message

--- src/lib/sitemapParser.js
+++ src/lib/sitemapParser.js
@@ -1,18 +1,18 @@
 import { slugify } from "./slugify";
-import { workforceSolutions, industriesWeServe, itSolutions } from "./sitemapData";
+import { WORKFORCE_SOLUTIONS, INDUSTRIES_WE_SERVE, IT_SOLUTIONS } from "./sitemapData";
 
 export function buildAllRoutes() {
-  const workforce = workforceSolutions.map((x) => ({
+  const workforce = WORKFORCE_SOLUTIONS.map((x) => ({
     url: `/workforce-solutions/${slugify(x.title)}`,
     title: x.title,
   }));
 
-  const industries = industriesWeServe.map((x) => ({
+  const industries = INDUSTRIES_WE_SERVE.map((x) => ({
     url: `/industries-we-serve/${slugify(x.title)}`,
     title: x.title,
   }));
 
-  const it = itSolutions.map((x) => ({
+  const it = IT_SOLUTIONS.map((x) => ({
     url: `/it-solutions/${slugify(x.title)}`,
     title: x.title,
   }));
